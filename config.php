9aa407aebe04f9c21a09e610520f7460523b8033
haliputri <77369540+haliputri@users.noreply.github.com>
2021-05-12 00:02:11 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$koneksi = mysqli_connect("localhost","root","","cobain");      // $koneksi = variable koneksi, itu bisa apa aja namanya
+/*
+if(!$koneksi){                                                  // kalo $koneksi false
+    echo "Gagal melakukan koneksi ke MySQL : ";
+} else {                                                        // kalo true
+    echo "berhasil sist";
+}
+*/
+?>
---
+++ login.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+<!--===============================================================================================-->
+</head>
+
+<body>
+	
+	<div class="limiter">
+		<!-- Baground warna warni gede -->
+		<div class="container-login100" style="background-color: #A8E3D7;">
+			<!-- Backgound putih login -->
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<!-- Formulir untuk login -->
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-49">Login</span>
+
+					<div class="tiga-opsi-user">
+						<ul>
+							<li class="admin"><a href="#">Admin</a></li>
+							<li><a href="#">Dosen</a></li>
+							<li><a href="#">Mahasiswa</a></li>
+						</ul>
+						<div class="under-line"></div>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+						<span class="label-input100">Username</span>
+						<!-- tempat input username -->
+						<input class="input100" type="text" name="username" placeholder="Type your username">
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<span class="label-input100">Password</span>
+						<!-- tempat input password -->
+						<input class="input100" type="password" name="pass" placeholder="Type your password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+					<!-- link kalo lupa password -->
+					<div class="text-right p-t-8 p-b-31">
+						<a href="#">Forgot password?</a>
+					</div>
+					
+					<!-- button login -->
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">Login</button>
+						</div>
+					</div>
+
+					<!-- kalo misalnya mau sign up manual -->
+					<div class="flex-col-c p-t-155">
+						<span class="txt1 p-b-17">Or Sign Up Using</span>
+						<!-- link ke page untuk sign up -->
+						<a href="#" class="txt2">Sign Up</a>
+					</div>
+				</form>
+				
+				<!-- masukin php -->
+				<?php
+					
+				
+				?>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
