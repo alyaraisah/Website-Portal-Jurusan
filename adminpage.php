bc2dfb302bc467195951c983d3de3145725ea9cc
nabilfikrisp <mnabilfikrisp@gmail.com>
2021-05-12 16:00:24 +0700

    nambah admin page

---
+++ adminpage.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400&display=swap" rel="stylesheet">
+
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Portal Web FMIPA Unpad</title>
+  </head>
+  <body>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#172D44;">
+      <div class="container">
+        <a class="navbar-brand" href="#">Portal Web FMIPA Unpad</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"><i class="fa fa-home"></i></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fa fa-globe"></i>
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">English (en)</a></li>
+                <li><a class="dropdown-item" href="#">Indonesia (id)</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-secondary tombol" href="landingPage.php">Keluar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- akhir navbar -->
+
+
+    <!-- Container -->
+    <div class="container">
+    <h4 class="text-center m-4">DATA JURUSAN</h4>
+              <div class="row">
+                <div class="card mx-3 mb-3 " style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+                  <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+                <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+                <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+
+              <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+                <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+                  <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+                <div class="card mx-3 mb-3" style="width: 18rem;">
+                    <img src="img/img3.png" class="card-img-top" alt="img/img3.png">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#">NAMA JURUSAN</a></p>
+                        </div>
+                </div>
+    </div>
+
+    <!-- Akhir Container -->
+
+    <!-- Jumbutron footer -->
+     <div class="jumbotron-footer jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4-footer"><span><br><br>HUBUNGI KAMI</span>
+
+          <br><br>Telp: 085759314557; 085225999797
+          <br>Email: elearning@unpad.ac.id
+          <br>Office: Gedung Graha Kandaga, Lt 4.
+          <br>Kampus Unpad Jatinagor, Sumedang, 45363
+          <span></span> </h1>
+        <p class="lead-footer text-center"><br>© UNIVERSITAS PADJADJARAN All rights reserved</p>
+      </div>
+    </div>
+    <!-- akhir footer -->
+
+    <!-- header 
+    <header>
+      <h1><a href="index.html">Portal Web FMIPA Unpad</a></h1>
+      <ul>
+          <li><a href="login.html">Log In</a></li>
+      </ul>
+    </header>  
+    -->
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
