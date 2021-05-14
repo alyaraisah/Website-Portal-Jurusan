a0a50acf78d03fb100722bffeac01dfecd511da2
nabilfikrisp <mnabilfikrisp@gmail.com>
2021-05-14 14:08:44 +0700

    dekor

--- pagemahasiswa.php
+++ pagemahasiswa.php
@@ -45,17 +45,49 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
+
+        <!-- drop down actions -->
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item dropdown">
+            <a class="btn btn-outline-light dropdown-toggle me-2" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+              Actions
+            </a>
+
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="tambahakun.php">Buat Akun</a></li>
+                <li><a class="dropdown-item" href="tambahmahasiswa.php">Tambah Mahasiswa</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+            <a class="btn btn-outline-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+              Data
+            </a>
+
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="pageakun.php">Daftar Akun</a></li>
+                <li><a class="dropdown-item" href="pagedosen.php">Daftar Dosen</a></li>
+                <li><a class="dropdown-item" href="pagekurikulum.php">Daftar Kurikulum</a></li>
+              </ul>
+            </li>
+
+
+        <!-- search -->
         <div class="container-fluid">
             <form class="d-flex" actions="" method="post">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="keyword" autocomplete="off">
-            <button class="btn btn-outline-success" type="submit" name="cari">Search</button>
+            <button class="btn btn-outline-light" type="submit" name="cari">Search</button>
              </form>
          </div>
+
+        <!-- home sama globe -->
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#"><i class="fa fa-home"></i></a>
             </li>
+
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="fa fa-globe"></i>
@@ -76,32 +108,12 @@
 
 <div class="container">
     <!-- content -->
-    <h1>Daftar Mahasiswa</h1>
-    <h2>
-        <a href="pageakun.php">Daftar Akun</a>
-        <a href="pagedosen.php">Daftar Dosen</a>
-        <a href="pagekurikulum.php">Daftar Kurikulum</a>
-    </h2>
-    <a href="tambahakun.php">Buat Akun</a>
-    <a href="tambahmahasiswa.php">Tambah Data Mahasiswa</a>
-    <br><br>
-    <!-- <form actions="" method="post">
-        <input type="text" name="keyword" size="30" autofocus placeholder="Input keyword..." autocomplete="off">
-        <button type="submit" name="cari">Cari!</button>
-    </form>
-    <br> -->
+    <h1 class="text-center" style="color:#172D44;">Daftar Mahasiswa</h1>
 
     <table border="1" cellpadding="10" cellspacing="0" class="table">
         <tr>
-            <!-- <th>No.</th>
-            <th>Aksi</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>NPM</th>
-            <th>Jurusan</th>
-            <th>Akun</th> -->
 
-            <thead>
+            <thead style="background-color:#172D44;" class="text-white">
                 <tr>
                 <th scope="col">No.</th>
                 <th scope="col">Aksi</th>
@@ -118,8 +130,8 @@
         </tr>
             <td><?= $i;?></td>
             <td>
-                <a href="hapus.php?no_mhs=<?=$row["no_mhs"];?>" onclick="return confirm('yakin?');">Hapus</a> | 
-                <a href="edit.php?no_mhs=<?=$row["no_mhs"];?>">Edit</a>
+                <a href="hapus.php?no_mhs=<?=$row["no_mhs"];?>" onclick="return confirm('yakin?');" class="btn btn-outline-primary fs-6 text-decoration-none">Hapus</a> | 
+                <a href="edit.php?no_mhs=<?=$row["no_mhs"];?>" class="btn btn-outline-primary fs-6 text-decoration-none">Edit</a>
             </td>
             <td><?= $row["NamaMhs"]?></td>
             <td><?= $row["Alamat"]?></td>
@@ -132,5 +144,8 @@
     <!-- akhir content -->
 </div>
 <!-- akhir container -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 </body>
 </html> 
